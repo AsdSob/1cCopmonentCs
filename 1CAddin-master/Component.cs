c8a93b204c77b3bf3e9343f082cc2527eac3f5d8
SobirASD <34741911+SobirASD@users.noreply.github.com>
2020-11-06 10:49:30 +0500

    base COM
    при зборке проверка на 1с
    	резултат = ПодключитьВнешнююКомпоненту("AddIn.1C_Component");
    
    	test= Новый COMОбъект("AddIn.1C_Component");
    	Сообщить(test.Procedure(1));

---
+++ 1CAddin-master/Component.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace System1C.AddIn
+{
+    /// <summary>Класс, реализующий пользовательские методы компоненты</summary>
+    [ProgId("AddIn.1C_Component")]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class Component : AddIn, IComponent
+    {
+        public int Procedure(int parameter)
+        {
+            return parameter + 100;
+        }
+    }
+}
